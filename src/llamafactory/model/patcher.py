9480d44cc259afadb74e461ecf584d883b5a5909
Ronald Seoh <ronaldseoh@icloud.com>
2024-11-20 23:36:05 -0500

    patch_processor() seems to be expecting non-existent values

--- src/llamafactory/model/patcher.py
+++ src/llamafactory/model/patcher.py
@@ -63,15 +63,54 @@ def patch_processor(
     tokenizer: "PreTrainedTokenizer",
     model_args: "ModelArguments",
 ) -> None:
-    setattr(processor, "tokenizer", tokenizer)
-    setattr(processor, "image_seqlen", get_image_seqlen(config))
-    setattr(processor, "image_resolution", model_args.image_resolution)
-    setattr(processor, "patch_size", get_patch_size(config))
-    setattr(processor, "video_resolution", model_args.video_resolution)
-    setattr(processor, "video_fps", model_args.video_fps)
-    setattr(processor, "video_maxlen", model_args.video_maxlen)
-    setattr(processor, "vision_feature_select_strategy", get_vision_feature_select_strategy(config))
+    
+    try:
+        setattr(processor, "tokenizer", tokenizer)
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        pass
+        
+    try:
+        setattr(processor, "image_seqlen", get_image_seqlen(config))
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        pass
 
+    try:
+        setattr(processor, "image_resolution", model_args.image_resolution)
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        pass
+
+    try:
+        setattr(processor, "patch_size", get_patch_size(config))
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        pass
+        
+    try:
+        setattr(processor, "video_resolution", model_args.video_resolution)
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        pass
+
+    try:
+        setattr(processor, "video_fps", model_args.video_fps)
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        pass
+        
+    try:
+        setattr(processor, "video_maxlen", model_args.video_maxlen)
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        pass
+
+    try:
+        setattr(processor, "vision_feature_select_strategy", get_vision_feature_select_strategy(config))
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        pass
 
 def patch_config(
     config: "PretrainedConfig",
