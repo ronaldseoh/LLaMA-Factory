086d86863ef4252a1cc6cbbfdd81c63cc0e5c747
Ronald Seoh <ronaldseoh@icloud.com>
2026-03-18 15:04:23 -0400

    Reapply "debug"
    This reverts commit f56911c3cd27d25105fb70dc9f60ed754d4ced2f.

--- src/llamafactory/cli.py
+++ src/llamafactory/cli.py
@@ -12,6 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import multiprocessing as mp
+
+# --- ADD THIS BLOCK ---
+try:
+    mp.set_start_method('spawn', force=True)
+except RuntimeError:
+    pass
+
 
 def main():
     from .extras.misc import is_env_enabled
