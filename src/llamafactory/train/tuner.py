43ec1bdf8d4469ef39e11f36395a2cb03b5888d7
Ronald Seoh <ronaldseoh@icloud.com>
2026-04-04 17:02:53 -0400

    troubleshooting

--- src/llamafactory/train/tuner.py
+++ src/llamafactory/train/tuner.py
@@ -17,6 +17,7 @@ import shutil
 from typing import TYPE_CHECKING, Any, Optional
 
 import torch
+torch.multiprocessing.set_sharing_strategy('file_system')
 import torch.distributed as dist
 from transformers import EarlyStoppingCallback, PreTrainedModel
 
